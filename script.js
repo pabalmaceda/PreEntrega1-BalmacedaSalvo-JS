4727a85b03d63682ae59d2706a0ccb556d6ddbf1
balmaceda <salvo_matias@hotmail.com>
2023-12-25 15:20:33 -0300

    funcion de orden superior y objeto persona

--- script.js
+++ script.js
@@ -1,40 +1,82 @@
-function calcularEdadPromedio() {
-  let continuar;
-  
-  do {
+function Persona(nombre, edad) {
+  this.nombre = nombre;
+  this.edad = edad;
+}
 
-    let cantidadPersonas;
+function calcularPromedioEdades(personas) {
+  let totalEdades = 0;
+
+  for (let i = 0; i < personas.length; i++) {
+    totalEdades += personas[i].edad;
+  }
+
+  return totalEdades / personas.length;
+}
+
+function mostrarInformacion(personas, promedio) {
+  console.log("Información de las personas:");
+
+  for (let i = 0; i < personas.length; i++) {
+    console.log(`Nombre: ${personas[i].nombre}, Edad: ${personas[i].edad}`);
+  }
+
+  console.log(`El promedio de edades es: ${promedio}`);
+}
+
+function filtrarPersonasMayores(personas, edadLimite) {
+  return personas.filter(persona => persona.edad > edadLimite);
+}
+
+
+function encontrarPersonaMayorEdad(personas) {
+  return personas.reduce((personaMayor, personaActual) => {
+    return personaActual.edad > personaMayor.edad ? personaActual : personaMayor;
+  });
+}
+
+function calcularEdadPromedio() {
+  const personas = [];
+  let cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas que desea ingresar:"));
+
+  while (isNaN(cantidadPersonas) || cantidadPersonas <= 0) {
+    alert("Por favor, ingrese un número válido de personas.");
+    cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas que desea ingresar:"));
+  }
+
+  let continuar;
+
+  do {
+    let nombre = prompt("Ingrese el nombre de la persona:");
+    let edad;
 
     do {
-      cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas (debe ser más de 1):"));
+      edad = parseInt(prompt("Ingrese la edad de la persona:"));
 
-      if (isNaN(cantidadPersonas) || cantidadPersonas <= 1) {
-        alert("Por favor, ingrese un número válido de personas.");
+      if (isNaN(edad) || edad <= 0) {
+        alert("Por favor, ingrese una edad válida.");
       }
-    } while (isNaN(cantidadPersonas) || cantidadPersonas <= 1);
+    } while (isNaN(edad) || edad <= 0);
 
-    let totalDeEdades = 0;
+    const nuevaPersona = new Persona(nombre, edad);
+    personas.push(nuevaPersona);
 
-    for (let i = 0; i < cantidadPersonas; i++) {
-      let edad;
-
-      do {
-        edad = parseInt(prompt(`Ingrese la edad de la persona ${i + 1}:`));
-
-        if (isNaN(edad) || edad <= 0) {
-          alert("Por favor, ingrese una edad válida.");
-        }
-      } while (isNaN(edad) || edad <= 0);
-
-      totalDeEdades += edad;
+    if (personas.length === cantidadPersonas) {
+      continuar = false;
+    } else {
+      continuar = confirm("¿Desea ingresar otra persona?");
     }
+  } while (continuar);
 
-    const promedio = totalDeEdades / cantidadPersonas;
+  const promedio = calcularPromedioEdades(personas);
 
-    alert(`La edad promedio de las ${cantidadPersonas} personas registradas es: ${promedio}`);
+  mostrarInformacion(personas, promedio);
 
-    continuar = confirm ("¿Desea realizar otro cálculo?");
-    }
-  while (continuar)
+  const personasMayores = filtrarPersonasMayores(personas, 30);
+  console.log("Personas mayores a 30 años:", personasMayores);
+
+
+  const personaMayorEdad = encontrarPersonaMayorEdad(personas);
+  console.log("Persona de mayor edad:", personaMayorEdad);
 }
-  calcularEdadPromedio();
+
+calcularEdadPromedio();
